3e11d82b75deba1fc68e5bf53076188fabce12ff
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-10-23 18:58:54 +0600

    peach: Some edits

--- cm.mk
+++ cm.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit from wt88047 device
+# Inherit from peach device
 $(call inherit-product, device/ark/peach/device.mk)
 
 # Must define platform variant before including any common things
